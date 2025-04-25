a2ea92e4907a0a6f0c441ddf5438457745d0f1ab
maratkasyanov1 <onouhjioj@gmail.com>
2025-04-25 04:59:46 +0300

    test

--- src/components/UI/chat/chat.tsx
+++ src/components/UI/chat/chat.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Box, useMediaQuery, useTheme } from '@mui/material';
 import DoctorChat from './DoctorChat';
-import Header from './Header';
+import Header from '../header';
 import { useNavigate } from 'react-router-dom';
 
 const Chat = () => {
